f552abd1e6c736b0eb7fb39a77b439884b2885a2
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2019-04-03 07:43:26 +0300

    Added two Python 3 scripts

---
+++ py2.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+if len(sys.argv) >= 2:
+	directory = str(sys.argv[1])
+else:
+	print('Not enough arguments!')
+	sys.exit(0)
+
+total = 0
+
+for root, dirs, files in os.walk(directory):
+	for file in files:
+		total = total + 1
+
+print('Visited', total, 'files!')
